eae10d185460ab3a64342ca35c91c7748b8a1836
pieea <pieea@daum.net>
2013-07-21 20:32:30 +0900

    zzzzz

--- notificationtutorial/src/com/androidbegin/MainActivity.java
+++ notificationtutorial/src/com/androidbegin/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		//zzzzzzzzzzzz
+		//zzzzzzzzzzzzzzzz
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
